742efbca9262927896edd1d79c15c7f84cca3fa8
Andrew Cottage <andcott@gmail.com>
2015-05-09 10:03:49 -0400

    added more finder tests

--- test/roto_test.rb
+++ test/roto_test.rb
@@ -4,13 +4,32 @@ require_relative "../lib/roto.rb"
 class TestRoto < Minitest::Test
   def setup
     @roto = Roto.new
+    @path = "#{Dir.pwd}/test/fixtures"
   end
-  def test_that_finder_can_find
-    path = "#{Dir.pwd}/test/fixtures"
+  def test_that_finder_can_find_jpgs    
     filetypes=['jpg']
-    photos = @roto.find_photos(path, filetypes)
-    assert_equal ["#{Dir.pwd}/test/fixtures/test.jpg"], photos
+    photos = @roto.find_photos(@path, filetypes)
+    assert_equal ["#{@path}/test.jpg"], photos
   end
+
+  def test_that_finder_can_find_pngs
+    filetypes=['png']
+    photos = @roto.find_photos(@path, filetypes)
+    assert_equal ["#{@path}/test.png"], photos
+  end
+
+  def test_that_finder_can_find_mp4s
+    filetypes=['mp4']
+    photos = @roto.find_photos(@path, filetypes)
+    assert_equal ["#{@path}/test.mp4"], photos
+  end
+
+  def test_that_finder_can_find_mutiple_filetypes
+    filetypes=['mp4', 'jpg', 'png']
+    photos = @roto.find_photos(@path, filetypes)
+    assert_equal ["#{@path}/test.mp4", "#{@path}/test.jpg", "#{@path}/test.png"], photos
+  end
+
 
   def test_that_mover_can_move
   end
