c40702c179786b35d2cbdfa289fc6dc195933a2e
Andrew Cottage <andcott@gmail.com>
2017-03-26 11:33:51 -0400

    fix copy and move bug

--- lib/roto.rb
+++ lib/roto.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 require 'find'
 require 'ruby-progressbar'
+require 'logger'
 
 class Roto
   attr_accessor :types, :files, :rename_duplicates
@@ -11,16 +12,18 @@ class Roto
     @types = ['.mp4', '.mov', '.jpg', '.png', '.mts']
     @rename_duplicates = true
     @errors = {}
+    @logger = Logger.new(STDOUT)
   end
 
   def find_files(path)
-    progressbar = ProgressBar.create(total: nil, format: "%a Photos Found: %c")
+    progressbar = ProgressBar.create(total: nil, format: "%a Finding Files")
     Find.find(path).each do |file|
       if @types.include?(File.extname(file).downcase)
         @files << file
         progressbar.increment
       end
     end
+    @logger.info("Found: #{@files.count} files")
   end
 
   def move_files(destination)
@@ -32,12 +35,15 @@ class Roto
       begin
         if File.exist?("#{destination}/#{filename}") && @rename_duplicates
           FileUtils.mv("#{file}", "#{destination}/#{name}_#{Time.now.to_i}#{ext}")
+        else
+          FileUtils.mv("#{file}", "#{destination}")
         end
         progressbar.increment
       rescue => error
         @errors[file] = error
       end
     end
+    self
   end
 
   def copy_files(destination)
@@ -49,6 +55,8 @@ class Roto
       begin
         if File.exist?("#{destination}/#{filename}") && @rename_duplicates
           FileUtils.cp("#{file}", "#{destination}/#{name}_#{Time.now.to_i}#{ext}")
+        else
+          FileUtils.cp("#{file}", "#{destination}")
         end
         progressbar.increment
       rescue => error
@@ -56,4 +64,5 @@ class Roto
       end
     end
   end
+  self
 end
