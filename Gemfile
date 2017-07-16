a57061c4ed57ffe41e496b7e11edfc99f180cc3d
Andrew Cottage <andcott@gmail.com>
2017-07-16 12:49:01 -0400

    tests

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'minitest'
 gem 'ruby-progressbar'
+gem 'rake'
--- test/roto_test.rb
+++ test/roto_test.rb
@@ -22,40 +22,40 @@ class TestRoto < Minitest::Test
 
   def test_that_finder_can_find_jpgs
     filetypes=['jpg']
-    photos = @roto.find_photos(@fixtures_path, filetypes)
+    photos = @roto.find_files(@fixtures_path)
     assert_equal ["#{@fixtures_path}/test.jpg"], photos
   end
 
   def test_that_finder_can_find_pngs
     filetypes=['png']
-    photos = @roto.find_photos(@fixtures_path, filetypes)
+    photos = @roto.find_files(@fixtures_path)
     assert_equal ["#{@fixtures_path}/test.png"], photos
   end
 
   def test_that_finder_can_find_mp4s
     filetypes=['mp4']
-    photos = @roto.find_photos(@fixtures_path, filetypes)
+    photos = @roto.find_files(@fixtures_path)
     assert_equal ["#{@fixtures_path}/test.mp4"], photos
   end
 
   def test_that_finder_can_find_mutiple_filetypes
     filetypes=['mp4', 'jpg', 'png']
-    photos = @roto.find_photos(@fixtures_path, filetypes)
+    photos = @roto.find_files(@fixtures_path)
     assert_equal ["#{@fixtures_path}/test.mp4", "#{@fixtures_path}/test.jpg", "#{@fixtures_path}/test.png"], photos
   end
 
 
   def test_that_mover_can_move
-    files = @roto.find_photos(@fixtures_path, @filetypes)
+    files = @roto.find_files(@fixtures_path)
     @roto.move_files(files, @moved_files_path)
-    assert_equal ["test.mp4", "test.jpg", "test.png"], @roto.find_photos(@moved_files_path, @filetypes).map {|path| File.basename path}
+    assert_equal ["test.mp4", "test.jpg", "test.png"], @roto.find_files(@moved_files_path, @filetypes).map {|path| File.basename path}
   end
 
   def test_that_copier_can_copy
-    files = @roto.find_photos(@fixtures_path, @filetypes)
-    @roto.copy_photos(files, @moved_files_path)
-    assert_equal ["test.mp4", "test.jpg", "test.png"], @roto.find_photos(@moved_files_path, @filetypes).map {|path| File.basename path}
-    assert_equal ["test.mp4", "test.jpg", "test.png"], @roto.find_photos(@fixtures_path, @filetypes).map {|path| File.basename path}
+    files = @roto.find_files(@fixtures_path)
+    @roto.copy_files(files, @moved_files_path)
+    assert_equal ["test.mp4", "test.jpg", "test.png"], @roto.find_files(@moved_files_path, @filetypes).map {|path| File.basename path}
+    assert_equal ["test.mp4", "test.jpg", "test.png"], @roto.find_files(@fixtures_path, @filetypes).map {|path| File.basename path}
   end
 
   def test_that_duplicates_are_renamed
@@ -63,9 +63,9 @@ class TestRoto < Minitest::Test
     FileUtils.touch("#{@duplicates_path}/test.png")
     FileUtils.touch("#{@duplicates_path}/test.mp4")
     files = []
-    files << @roto.find_photos(@fixtures_path, @filetypes)
-    files << @roto.find_photos(@duplicates_path, @filetypes)
-    @roto.copy_photos(files.flatten, @moved_files_path)
-    assert_equal ["test.mp4", "test.jpg", "test.png", "test_2.mp4", "test_2.jpg", "test_2.png"], @roto.find_photos(@moved_files_path, @filetypes).map {|path| File.basename path}
+    files << @roto.find_files(@fixtures_path)
+    files << @roto.find_files(@duplicates_path)
+    @roto.copy_files(@moved_files_path)
+    assert_equal ["test.mp4", "test.jpg", "test.png", "test_2.mp4", "test_2.jpg", "test_2.png"], @roto.find_files(@moved_files_path, @filetypes).map {|path| File.basename path}
   end
 end
